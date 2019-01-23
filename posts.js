f43919e6e73c68e35cea5beb0560e3895293a91e
cptn-neemo <maxhuddleston@gmail.com>
2019-01-22 21:55:05 -0600

    Add documentation

--- posts.js
+++ posts.js
@@ -4,12 +4,23 @@ const moment = require('moment');
 require('dotenv').config();
 const searchTerms = ["IR", "Iron Ranger", "Wolverine", "1k", "Red Wing", "RW"];
 
-
+/**
+ * Fetch the posts from the reddit api
+ * @return { Promise } fetch request to fmf subreddit
+ */
 const getPosts = () => {
     return fetch('https://reddit.com/r/frugalmalefashion/new.json')
         .then(resp => resp.json())
 }
 
+/**
+ * Parse the posts returned by the api fetch request
+ * Check that the recently posted threads are within the time limit,
+ * then use the regex to check if they contain the search terms
+ * If term is contained in title or body, send an alert email
+ * 
+ * @param { Object } body fetch request body
+ */
 const parsePosts = async (body) => {
     const posts = body.data.children.map(obj => obj.data);
     const lowerBound = moment().subtract(process.env.lowerBound, 'minutes');
@@ -28,6 +39,13 @@ const parsePosts = async (body) => {
     })
 }
 
+/**
+ * Send the email alert containing info on the deal
+ * 
+ * @param { Object } post reddit api thread data
+ * @param { String } term search term contained in the title or text of post
+ * @return { Object } nodemailer info on the sent email
+ */
 const sendAlert = async (post, term) => {
     const body = `
         <h1>Alert: fmf post about ${term}</h1> 
@@ -42,14 +60,15 @@ const sendAlert = async (post, term) => {
             pass: process.env.pass
         }
     });
+
     const mailOptions = {
         from: 'fmfalerts@gmail.com',
         to: process.env.email,
         subject: "Alert for " + term,
         html: body 
     };
-    const info = await transporter.sendMail(mailOptions);
-    return info;
+
+    return await transporter.sendMail(mailOptions);
 }
 
 module.exports = async () => {
